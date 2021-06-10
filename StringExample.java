2b355af7a67a8efc5d781555f35e101199188f4d
sathitp26 <47035850+sathitp26@users.noreply.github.com>
2021-06-10 13:40:29 +0700

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,12 @@
+package Helloworld;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		char[] myChar = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+		String myWord = new String(myChar);
+		System.out.println(myWord);
+
+	}
+
+}
