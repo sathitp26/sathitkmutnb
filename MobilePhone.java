2b921c6160ebce41d381b3494e91a5d5f0cdc7de
sathitp26 <47035850+sathitp26@users.noreply.github.com>
2021-06-10 16:11:01 +0700

    Add files via upload

---
+++ MobilePhone.java
@@ -0,0 +1,26 @@
+package Helloworld;
+
+public class MobilePhone {
+  void PhoneCall() {}
+  void SMS() {}
+  void internetSurfing() {}
+}
+class SumsungGalaxyNote8 extends MobilePhone{
+	void UseGearVR() {}
+	void TranformToPC() {}
+	void UsePen() {}
+	void GooglePlay() {}
+}
+class IPhoneX extends MobilePhone{
+	void FaceID() {}
+	void AppStore() {}
+}
+class IPhone8 extends MobilePhone{
+	void TouchID() {}
+	void AppStore() {}
+}
+class SumsungGalaxyS8 extends MobilePhone{
+	void UseGearVR() {}
+	void TranformToPC() {}
+	void GooglePlay() {}
+}
